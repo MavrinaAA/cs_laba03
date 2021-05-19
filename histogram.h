9bf779ce6e2548dadca3b5f4480878c892ae03fb
Alena(MavrinaAA) <alenamavrina93@gmail.com>
2021-05-19 19:12:31 +0300

    code:перенос функций

--- histogram.h
+++ histogram.h
@@ -1,9 +1,15 @@
 #pragma once
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 void
 find_minmax(const vector<double>& numbers, double& min, double& max);
 
+vector<size_t>
+make_histogram (const vector<double>& numbers,size_t bin_count);
+
+void
+show_histogram_text (const vector<size_t>& bins);
 
