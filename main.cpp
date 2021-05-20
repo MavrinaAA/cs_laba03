d2807dce472f7f6812baeb3b766c3e10aed0aa8b
Alena(MavrinaAA) <alenamavrina93@gmail.com>
2021-05-20 22:09:33 +0300

    3.2.9 Сохранение данных из сети в буфер

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include "histogram.h"
 #include "svg.h"
 #include <curl/curl.h>
+#include <sstream>
+#include <string>
 using namespace std;
 
 vector<double>
@@ -42,27 +44,44 @@ read_input(istream& in,bool prompt)
     return data;
 }
 
+size_t
+write_data(void* items, size_t item_size, size_t item_count, void* ctx) {
+    size_t data_size=item_size*item_count;
+    stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
+   buffer->write(reinterpret_cast<const char*>(items), data_size);
+    return data_size;
+}
 
-int
-main(int argc, char* argv[]) {
+Input
+download(const string& address) {
+    stringstream buffer;
     curl_global_init(CURL_GLOBAL_ALL);
-    if (argc>1)
-    {
-        CURL *curl = curl_easy_init();
+    CURL *curl = curl_easy_init();
         if(curl)
         {
             CURLcode res;
-            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+            curl_easy_setopt(curl, CURLOPT_URL, address.c_str());
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
             res = curl_easy_perform(curl);
-            curl_easy_cleanup(curl);
             if (res)
             {
                 cerr << curl_easy_strerror(res) << endl;
                 exit(1);
             }
         }
+    curl_easy_cleanup(curl);
+    return read_input(buffer, false);
+}
+
+int
+main(int argc, char* argv[]) {
+    Input input;
+    if (argc > 1) {
+        input = download(argv[1]);
+    } else {
+        input = read_input(cin, true);
     }
-    const auto input=read_input(cin,true);
     const auto bins=make_histogram(input);
 
     // Вывод данных
