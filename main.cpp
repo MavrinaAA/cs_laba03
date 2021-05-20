786baad7d6a6a7f5e82102c5cf92bdfa71b0277d
Alena(MavrinaAA) <alenamavrina93@gmail.com>
2021-05-20 11:14:28 +0300

    3.2.5 Получение аргументов программы

--- main.cpp
+++ main.cpp
@@ -44,7 +44,15 @@ read_input(istream& in,bool prompt)
 
 
 int
-main() {
+main(int argc, char* argv[]) {
+    if (argc>1)
+    {
+        cout<<argc<<"\n";
+        for (size_t i=0;i<argc;i++)
+        {
+            cout<<"argv["<<i<<"]="<<argv[i]<<"\n";
+        }
+    }
     curl_global_init(CURL_GLOBAL_ALL);
     const auto input=read_input(cin,true);
     const auto bins=make_histogram(input);
