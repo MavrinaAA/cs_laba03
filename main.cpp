25d4b5b2b597337893c345356d80e17d6d8f7c73
Alena(MavrinaAA) <alenamavrina93@gmail.com>
2021-05-23 14:09:39 +0300

    3.2 print windows version

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include "histogram.h"
 #include "svg.h"
+#include <windows.h>
 using namespace std;
 
 vector<double>
@@ -18,7 +19,7 @@ input_numbers(size_t count)
 int
 main() {
     // Ввод данных
-    size_t number_count;
+   /*size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
 
@@ -28,15 +29,19 @@ main() {
     size_t bin_count;
     cerr << "Enter column count: ";
     cin >> bin_count;
-
     // Обработка данных
     double min,max;
     find_minmax(numbers,min,max);
     const auto bins=make_histogram(numbers,bin_count);
 
     // Вывод данных
-    size_t bin_height;
+    /*size_t bin_height;
     show_histogram_svg(bins,bin_height);
+    */
+
+DWORD WINAPI GetVersion(void);
+printf("Windows (decimal) version is %u.\n", GetVersion());
+printf("WINDOWS (16x) version is %x. \n",GetVersion());
 
     return 0;
 }
