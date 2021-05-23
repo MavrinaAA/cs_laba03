5fafb9bc32c9365d6d9db0987c8609f860982c34
Alena(MavrinaAA) <alenamavrina93@gmail.com>
2021-05-23 16:14:55 +0300

    3.3 added make_info_text

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ input_numbers(size_t count)
 int
 main() {
     // Ввод данных
-   /*size_t number_count;
+   size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
 
@@ -30,31 +30,12 @@ main() {
     cerr << "Enter column count: ";
     cin >> bin_count;
     // Обработка данных
-    double min,max;
-    find_minmax(numbers,min,max);
     const auto bins=make_histogram(numbers,bin_count);
 
     // Вывод данных
-    /*size_t bin_height;
+    size_t bin_height;
     show_histogram_svg(bins,bin_height);
-    */
-DWORD info = GetVersion();
-     DWORD mask = 0x0000ffff;
-    DWORD version = info & mask;
-    DWORD platform = info >> 16;
-    DWORD mask_2 = 0x0000ff;
-   if ((info & 0x80000000) == 0)
-    {
-        DWORD version_major = version & mask_2;
-        DWORD version_minor = version >> 8;
-        DWORD build = platform;
-        printf("Windows v%u.%u (build %u) \n", version_major, version_minor, build);
-    }
-    char computer_name[MAX_COMPUTERNAME_LENGTH + 1];
-    DWORD size = MAX_COMPUTERNAME_LENGTH+1;
-    GetComputerNameA(computer_name, &size);
-    printf("Computer name: %s\n", computer_name);
-    return 0;
+
 
 
     return 0;
--- svg.h
+++ svg.h
@@ -1,7 +1,13 @@
 #pragma once
 #include <vector>
 #include <iostream>
+#include <sstream>
+#include <windows.h>
 using namespace std;
+
+string
+make_info_text();
+
 void
 svg_begin(double width, double height);
 
